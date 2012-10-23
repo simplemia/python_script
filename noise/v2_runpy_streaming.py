a9af607189cfa6cb0e585042267c4684f95b0976
simplemia <ydbiji0911@163.com>
2012-10-23 14:38:24 +0800

    Update noise/v2_runpy_streaming.py

--- noise/v2_runpy_streaming.py
+++ noise/v2_runpy_streaming.py
@@ -2,15 +2,13 @@
 
 import subprocess as sub
 
-#hdstreaming = "/usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming-0.20.2-cdh3u1.jar"
-#log_path = "/user/root/flume/log_access"
 
 
 #temp = [i.split() for i in open("todo_dir").readlines()]
 temp_dir=[""]
 
 for i in temp_dir:
-    cmd = 'hadoop jar "/usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming.jar" \
+    cmd = 'hadoop jar "hadoop-0.20/contrib/streaming/hadoop-streaming.jar" \
             -D mapred.reduce.tasks=50 -mapper "python noise.py" -reducer "cat" -file noise.py \
             -input %s -output %s'%(i,i.split("summary/")[1].split("*")[0])
     print cmd
