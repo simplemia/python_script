9f1c3b7df823c4983b0ab736e5ca9fcb59242451
simplemia <ydbiji0911@163.com>
2012-10-23 14:29:08 +0800

    Update noise/v2_runpy_streaming.py

--- noise/v2_runpy_streaming.py
+++ noise/v2_runpy_streaming.py
@@ -7,15 +7,10 @@ import subprocess as sub
 
 
 #temp = [i.split() for i in open("todo_dir").readlines()]
-temp_dir=["/production/userprofile/V3/summary/dsp/adx/tid/auto/2012/10/20/*",\
-        "/production/userprofile/V3/summary/dsp/adx/tid/manual/2012/10/20/*",\
-        "/production/userprofile/V3/summary/dsp/adx/tid/semiauto/2012/10/20/*",\
-        "/production/userprofile/V3/summary/dsp/tanx/tid/auto/2012/10/20/*",\
-        "/production/userprofile/V3/summary/dsp/tanx/tid/manual/2012/10/20/*",\
-        "/production/userprofile/V3/summary/dsp/tanx/tid/semiauto/2012/10/20/*"]
+temp_dir=[""]
 
 for i in temp_dir:
-    cmd = 'hadoop jar "/usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming-0.20.2-cdh3u1.jar" \
+    cmd = 'hadoop jar "/usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming.jar" \
             -D mapred.reduce.tasks=50 -mapper "python noise.py" -reducer "cat" -file noise.py \
             -input %s -output %s'%(i,i.split("summary/")[1].split("*")[0])
     print cmd
