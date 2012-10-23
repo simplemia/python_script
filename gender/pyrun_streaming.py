1906896bf090965a111a4cca8e6acf3177a46b4b
simplemia <ydbiji0911@163.com>
2012-10-23 14:39:31 +0800

    Update gender/pyrun_streaming.py

--- gender/pyrun_streaming.py
+++ gender/pyrun_streaming.py
@@ -2,12 +2,12 @@
 
 import subprocess as sub
 
-i = "/production/userprofile/user_category_summary_merge_addrelation/subset/2012/10/01/*"
+i = "*"
 
-cmd = 'hadoop jar "/usr/lib/hadoop-0.20/contrib/streaming/hadoop-streaming-0.20.2-cdh3u1.jar" \
+cmd = 'hadoop jar "hadoop-0.20/contrib/streaming/hadoop-streaming-0.20.2-cdh3u1.jar" \
         -D mapred.reduce.tasks=100 -mapper "python gender_map.py" -reducer "python gender_reduce.py" \
         -file gender_map.py -file gender_reduce.py \
-        -input %s -output %s'%(i,"/subset/2012/10/01/")
+        -input %s -output %s'%(i,"/")
 print cmd
 handle = sub.Popen("%s"%cmd,stdout=sub.PIPE,shell=True)
 print handle.stdout.read()
